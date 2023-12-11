ee4c99ff14a6fa6684da5a2d56c5c1947bfa2b40
FrancescoMazzocchi <123@email.it>
2023-12-11 17:53:47 +0100

    add array carosello header bar

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -6,7 +6,29 @@ export default {
     name: "PageHeader",
     data() {
         return {
-            store
+            store,
+            news: [
+                {
+                    img: "healthy-foods-150x150.webp",
+                    text: "the best healthy foods",
+                    time: "17.39"
+                },
+                {
+                    img: "winter-150x150.webp",
+                    text: "the best winter outfits",
+                    time: "18.25"
+                },
+                {
+                    img: "rice-ball-150x150.webp",
+                    text: "hygenic recipe to prepare rice",
+                    time: "20.30"
+                },
+                {
+                    img: "src/assets/meal-time-150x150.webp",
+                    text: "the best time to have a meal",
+                    time: "22.18"
+                },
+            ]
         }
     },
     methods: {
