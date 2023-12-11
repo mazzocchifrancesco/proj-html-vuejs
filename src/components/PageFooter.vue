1dfaae36fd4c552cb558c6119bd68f2b2606f023
FrancescoMazzocchi <123@email.it>
2023-12-11 16:44:35 +0100

    layout footer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -18,14 +18,36 @@ export default {
 </script>
 
 <template>
-    <div>
-        <h1>footer</h1>
-    </div>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-2 tempLink">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt, sequi.</div>
+                <div class="col-2 tempLink">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt, sequi.</div>
+                <div class="col-2 tempLink">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt, sequi.</div>
+                <div class="col-2 tempLink">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt, sequi.</div>
+                <div class="col-4 tempLink">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt, sequi.</div>
+            </div>
+            <div class="row">
+                <hr class="col-12">
+            </div>
+            <div>
+                logo + altro
+            </div>
+        </div>
+    </footer>
 </template>
 
 <style scoped lang="scss">
 // importo variabili
-// @use './styles/partials/variables' as *;
+@use '../styles/partials/variables' as *;
+
+footer {
+    background-color: $themeColorAccent;
+}
+
+.tempLink {
+    height: 10rem;
+}
 
 // ...qui eventuale SCSS di AppComponent
 </style>
